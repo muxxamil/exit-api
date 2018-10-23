d73ab1037a8f3737a4063618968cade3f4f46896
muxamil <muzamilejaz@zameen.com>
2018-10-23 21:56:44 +0500

    parse int added

--- models/LocationBooking.js
+++ models/LocationBooking.js
@@ -186,7 +186,7 @@ module.exports = function (sequelize, DataTypes) {
     
     LocationBooking.getLocationBookingBetweenDateRanges = (params) => {
 
-        params.endDate = moment.utc(params.endDate).subtract(1, 'seconds').valueOf();
+        params.endDate = moment.utc(parseInt(params.endDate)).subtract(1, 'seconds').valueOf();
         
         let whereClause = {
             [Op.or]: [
